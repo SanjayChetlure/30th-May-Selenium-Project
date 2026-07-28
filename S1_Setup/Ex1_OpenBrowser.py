2c8d8cf0514194082e3cf72b087ae94ee5d04870
SanjayChetlure <sanjay.chetlure@gmail.com>
2026-07-28 21:18:08 +0530

    28th july 2026

---
+++ S1_Setup/Ex1_OpenBrowser.py
@@ -0,0 +1,10 @@
+#from packageName import moduleName
+import time
+
+from selenium import webdriver
+
+# driver=webdriver.Chrome()       #moduleName.className() -> object creation
+# driver=webdriver.Firefox()
+driver=webdriver.Edge()
+
+time.sleep(5)                   #moduleName.fn(timeInSec)
